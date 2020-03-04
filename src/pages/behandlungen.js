af0f32d4602875138a5fb2c51adb11c7f2198473
Mattia Cendon <mattia@cendon.de>
2020-03-04 21:41:35 +0100

    Textänderung

--- src/pages/behandlungen.js
+++ src/pages/behandlungen.js
@@ -24,12 +24,6 @@ const ElementsPage = ({ data }, location) => {
       <article className="post-content page-template no-image">
         <div className="post-content-body">
           <h2>Unsere Behandlungen</h2>
-          <p>
-            Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida odio
-            porttitor sem non mi integer non faucibus ornare mi ut ante amet
-            placerat aliquet. Volutpat eu sed ante lacinia sapien lorem accumsan
-            varius montes viverra nibh in adipiscing blandit tempus accumsan.
-          </p>
         </div>
       </article>
       <div className="post-feed ">
