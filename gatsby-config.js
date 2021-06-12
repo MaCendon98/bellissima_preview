65f6457ac0085c5c60882b5063408a90db7bb5fe
Mattia C <mattia@cendon.de>
2021-06-12 12:39:04 +0200

    LOOL

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-99728303-1",
+        trackingId: "",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional
